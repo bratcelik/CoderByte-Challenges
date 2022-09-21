5e8191b73e0431a5bbaa231f10de5b90115951b4
Berat Sercan Çelik <bratcelik@gmail.com>
2022-09-21 20:27:20 +0300

    Added solutions TripleDouble and PatternChaser

---
+++ src/Hard/PatternChaser.java
@@ -0,0 +1,66 @@
+package Hard;
+
+import java.util.Scanner;
+
+/*
+ * Pattern Chaser
+ *
+ * Have the function PatternChaser(str) take str which will be a string and
+ * return the longest pattern within the string. A pattern for this challenge will be defined as:
+ * if at least 2 or more adjacent characters within the string repeat at least twice.
+ * So for example "aabecaa" contains the pattern aa,
+ * on the other hand "abbbaac" doesn't contain any pattern.
+ * Your program should return yes/no pattern/null.
+ * So if str were "aabejiabkfabed" the output should be yes abe.
+ * If str were "123224" the output should return no null.
+ * The string may either contain all characters (a through z only), integers, or both.
+ * But the parameter will always be a string type. The maximum length for the string being passed in will be 20 characters.
+ * If a string for example is "aa2bbbaacbbb" the pattern is "bbb" and not "aa".
+ * You must always return the longest pattern possible.
+ *
+ * @author Berat Sercan Çelik
+ * */
+
+
+public class PatternChaser {
+    public static String PatternChaser(String str) {
+        // code goes here
+        String result = "";
+        int len = 0;
+
+        for(int i = 0; i < str.length()-3; i++)
+        {
+            for(int j = 2; j <= (str.substring(i+j).length()); j++)
+            {
+                //System.out.println(i + " " + j);
+                String pattern = str.substring(i,i+j);
+                String subStr = str.substring(i+j);
+
+                if(subStr.contains(pattern))
+                {
+                    if(len < pattern.length())
+                    {
+                        len = pattern.length();
+                        result = pattern;
+                        //System.out.println(result);
+                    }
+                }
+            }
+        }
+
+        if(len > 0)
+        {
+            result = "yes " + result;
+            return result;
+        }
+
+
+        return "no null";
+    }
+
+    public static void main (String[] args) {
+        // keep this function call here
+        Scanner s = new Scanner(System.in);
+        System.out.print(PatternChaser(s.nextLine()));
+    }
+}
---
+++ src/Medium/TripleDouble.java
@@ -0,0 +1,82 @@
+package Medium;
+
+import java.util.Scanner;
+
+/*
+* Triple Double
+*
+* Have the function TripleDouble(num1,num2) take both parameters being passed,
+* and return 1 if there is a straight triple of a number at any place in
+* num1 and also a straight double of the same number in num2.
+*
+* For example:
+*
+* if num1 equals 451999277 and num2 equals 41177722899,
+* then return 1 because in the first parameter you have the straight triple 999 and
+* you have a straight double, 99, of the same number in the second parameter.
+* If this isn't the case, return 0.
+*
+* @author Berat Sercan Çelik
+* */
+
+public class TripleDouble {
+    public static int TripleDouble(int num1, int num2) {
+        // code goes here
+        int[] arr = new int[10];
+        int[] arr2 = new int[10];
+
+
+        int temp = num1 % 10;
+        int counter = 1;
+
+        while(num1 != 0 )
+        {
+            num1 = num1 / 10;
+            if(num1 % 10 == temp)
+            {
+                counter++; // 2
+                if(counter >= 3)
+                    arr[temp] = 1;
+            }
+            else
+            {
+                counter = 1;
+            }
+
+            temp = num1 % 10;
+        }
+
+        temp = num2 % 10;
+        counter = 1;
+
+        while(num2 != 0 )
+        {
+            num2 = num2 / 10;
+            if(num2 % 10 == temp)
+            {
+                counter++;
+                if(counter >= 2)
+                    arr2[temp] = 1;
+            }
+            else
+            {
+                counter = 1;
+            }
+
+            temp = num2 % 10;
+        }
+
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(arr[i] == 1 && arr2[i] == 1)
+                return 1;
+        }
+        return 0;
+    }
+
+    public static void main (String[] args) {
+        // keep this function call here
+        Scanner s = new Scanner(System.in);
+        System.out.print(TripleDouble(s.nextInt(),s.nextInt()));
+    }
+}
